4858c1fddbeed8dd318c775d8aecbefdbfd76f71
Roman Khol <khol@IEXPERT.LOCAL>
2026-08-21 09:22:13 +0200

    verecel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
